b0f3992072bccda6309790fa668193dd9e6a2d88
Edison Weik <eweik@uci.edu>
2017-04-30 14:29:28 -0700

    Update zombie_maze.py

--- zombie_maze.py
+++ zombie_maze.py
@@ -1,23 +1,13 @@
-# ------------------------------------------------------------------------------------------------
-# Copyright (c) 2016 Microsoft Corporation
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-# associated documentation files (the "Software"), to deal in the Software without restriction,
-# including without limitation the rights to use, copy, modify, merge, publish, distribute,
-# sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all copies or
-# substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
-# NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-# ------------------------------------------------------------------------------------------------
+# Sample Code for 10x10 Maze with 2 Zombies
+#
+# The mission in this code only lasts for 60 sec. But the zombies seem to die randomly without spawning after 15 sec. 
+# I'm having trouble getting the zombies to live longer and for them to spawn. 
+# Right now the walls of the maze are only 1 block tall - this is to allow me to edit and visualize the space easily from within the
+# game. But obviously when we start the learning I would the walls taller to prevent the zombies from easily jumping them.
+#
+#
 
-# Sample Code for 10x10 Maze with Zombie
+
 
 import MalmoPython
 import os
